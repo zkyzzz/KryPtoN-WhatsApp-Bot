9655334b6859b7aa7bd50e5e00839c30b66b2e16
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-10-29 14:50:20 +0700

    initial postgresql connections

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const { create, Client } = require('@open-wa/wa-automate')
 const { color } = require('./utils')
 const options = require('./utils/options')
 const msgHandler = require('./handler/message')
+const pg = require('pg')
+
+const database = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+database.connect()
+
+database.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row))
+  }
+  database.end()
+})
 
 const start = (client = new Client()) => {
     console.log('[DEV]', color('KryPtoN', 'yellow'))
